72af1e267a249581e7da7fb8761f1858ae975bf5
NomadDaniel <reachingdaniel@gmail.com>
2020-04-09 17:50:58 -0400

    seems to be working. commented the CDN script in the HTML to save some API requests

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,8 +24,6 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -46,6 +44,57 @@ const followersArray = [];
 
 */
 
+function GithubCard(object){
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const img = document.createElement('img');
+  img.src = object['avatar_url'];
+
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+
+  const name = document.createElement('h3');
+  name.classList.add('name');
+  name.textContent = object.login;
+
+  const username = document.createElement('p');
+  username.classList.add('username');
+  username.textContent = object.name;
+
+  const location = document.createElement('p');
+  location.textContent = `Location: ${object.location}`;
+
+  const profile = document.createElement('p');
+  profile.textContent = `Profile: `;
+
+  const githubLink = document.createElement('a');
+  githubLink.href = object['html_url'];
+  githubLink.textContent = object['html_url'];
+
+  const followers = document.createElement('p');
+  followers.textContent = `Followers: ${object.followers}`;
+
+  const following = document.createElement('p');
+  following.textContent = `Following: ${object.following}`;
+
+  const bio = document.createElement('p');
+  bio.textContent = `Bio: ${object.bio}`;
+
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  profile.appendChild(githubLink);
+
+  return card;
+}
+
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
@@ -53,3 +102,36 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+const cards = document.querySelector('.cards')
+
+axios.get('https://api.github.com/users/nomaddaniel')
+  .then((response) => {
+    console.log(response)
+    cards.appendChild(GithubCard(response.data))
+    })
+
+const instructors = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell']
+
+instructors.forEach(instructor => {
+  axios.get(`https://api.github.com/users/${instructor}`)
+  .then(response => {
+    cards.appendChild(GithubCard(response.data))
+  }) 
+})
+
+const followerurl = []
+
+axios.get('https://api.github.com/users/nomaddaniel/followers')
+  .then ((response) => {
+    console.log(response.data)
+    response.data.forEach(item => {
+      followerurl.push(item.url)
+    })
+    followerurl.forEach(urlItem => {
+      axios.get(urlItem)
+        .then ((response) => {
+          cards.appendChild(GithubCard(response.data));
+        })
+    })
+  }) 
